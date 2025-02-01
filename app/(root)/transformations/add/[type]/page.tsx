7eec7ab83c5a79306a1662dddc5c11c56d880b12
Ana <anapedrabcn@gmail.com>
2025-02-01 15:53:21 +0100

    fixed transformation

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+const AddTransformationTypePage = () => {
+  return <div>AddTransformationTypePage</div>;
 };
 
-export default page;
+export default AddTransformationTypePage;
