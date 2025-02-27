f0afb5bfd64915918fb0e3380658fcba7b943e38
Ana <anapedrabcn@gmail.com>
2025-02-27 22:54:43 +0100

    changed page.jsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -17,18 +17,22 @@ const Home = async ({ searchParams }: SearchParamProps) => {
           Unleash Your Creative Vision with Imaginify
         </h1>
         <ul className="flex-center w-full gap-20">
-          {navLinks.slice(1, 5).map((link) => (
-            <Link
-              key={link.route}
-              href={link.route}
-              className="flex-center flex-col gap-2"
-            >
-              <li className="flex-center w-fit rounded-full bg-white p-4">
-                <Image src={link.icon} alt="image" width={24} height={24} />
-              </li>
-              <p className="p-14-medium text-center text-white">{link.label}</p>
-            </Link>
-          ))}
+          {navLinks.slice(1, 5).map((link) => {
+            return (
+              <Link
+                key={link.route}
+                href={link.route}
+                className="flex-center flex-col gap-2"
+              >
+                <li className="flex-center w-fit rounded-full bg-white p-4">
+                  <Image src={link.icon} alt="image" width={24} height={24} />
+                </li>
+                <p className="p-14-medium text-center text-white">
+                  {link.label}
+                </p>
+              </Link>
+            );
+          })}
         </ul>
       </section>
 
